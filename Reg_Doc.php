dd150afae2485c042c8285b0dd7f8ca4a31bb91e
Usman233 <66175893+Usman233@users.noreply.github.com>
2020-05-30 21:38:23 +0500

    Add files via upload

---
+++ Reg_Doc.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<style>
+*{margin:0px; padding:5px; font-family:Helvetica, Arial, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password],input[type=email] {
+    width: 90%;
+    padding: 12px 20px;
+    margin: 8px 26px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+	font-size:16px;
+}
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+.sample {
+    width: 200px;
+	height:200px;
+    border-radius: 50%;
+}
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 26px;
+    border: none;
+    cursor: pointer;
+    width: 90%;
+	font-size:20px;
+}
+button:hover {
+    opacity: 0.8;
+}
+
+/* The Modal (background) */
+.modal {
+	display:none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.4);
+}
+
+/* Modal Content Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 4% auto 15% auto;
+    border: 1px solid #888;
+    width: 40%; 
+	padding-bottom: 30px;
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+.close:hover,.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    animation: zoom 0.6s
+}
+@keyframes zoom {
+    from {transform: scale(0)} 
+    to {transform: scale(1)}
+}
+
+
+
+
+
+	
+</style>
+
+
+</head>
+<body onload="document.getElementById('modal-wrapper').style.display='block'" >
+<a href="index.php"  style="width:200px; margin-top:200px; margin-left:160px;">
+Go Back</a>
+<h1 style="text-align:center; font-size:50px; color:#fff"> Registration Form</h1>
+
+
+<div id="modal-wrapper" class="modal">
+  
+  <form class="modal-content animate" action="../Uni_project/signup_doc.php" method="Post">
+        
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">&times;</span>
+      <img src="images/1.png" alt="sample" class="sample">
+      <h1 style="text-align:center">Registration Foam</h1>
+    </div>
+
+    <div class="container">
+      Name<input type="text" placeholder="Enter Username" name="uname" required><br>
+	  Email:<input type="email" placeholder="abc@gmail.com" name="Email" required><br>
+      Password:<input type="password" placeholder="Enter Password" name="pswd" required> <br> 
+	  City name:<input type="text" placeholder="Enter City name" name="city" required> <br>
+	  Specialization:<input type="text" name="type" placeholder="Dentist" Required>
+      
+	 <button type="submit">SignUp</button>
+	  </form>
+	  
+	  
+      
+    </div>
+    
+  
+  
+</div>
+
+
+
+</body>
+</html>
+
+
